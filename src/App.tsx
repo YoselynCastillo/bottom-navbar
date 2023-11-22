4e3ddf8d94e874a9492afa73e82ce277b353a76d
edixon-Lizcano <abatar.111@gmail.com>
2023-11-22 09:23:50 -0500

    Add animation for drawer in and out

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import BottomNavbar from "./BottomNavbar/BottomNavbar";
-import './App.css'
-
+import "./App.css";
+import { useState } from "react";
+import image from './assets/icons/IMG_3845 (1).jpg';
 function App() {
+  const [drawerOpen, setDrawerOpen] = useState(false);
 
   return (
-    <>
-      <BottomNavbar />
-    </>
-  )
+    <div className="App">
+      <div className={drawerOpen ? 'blur-background' : ''}>
+        <img src={image} alt="dfasd" width={400} height={700} />
+      </div>
+      <BottomNavbar setDrawerOpen={setDrawerOpen} drawerOpen={drawerOpen}/>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/BottomNavbar/BottomNavbar.tsx
+++ src/BottomNavbar/BottomNavbar.tsx
@@ -1,104 +1,128 @@
 import { useState, useEffect, useRef } from "react";
 import "./BottomNavbar.scss";
-import { data } from "./data"
+import { data } from "./data";
 import {
-    SellIcon, FinanceIcon, ShopIcon,
-    ChatIcon,
-    MoreIcon
+  SellIcon,
+  FinanceIcon,
+  ShopIcon,
+  ChatIcon,
+  MoreIcon,
 } from "../assets/icons";
 
+const BottomNavbar = ({ drawerOpen, setDrawerOpen }) => {
+  const [active, setActive] = useState("");
+  const [currenData, setCurrenData] = useState(
+    data[active] ? data[active].options : []
+  );
+  // const [drawerOpen, setDrawerOpen] = useState(false);
+  const drawerRef = useRef<HTMLDivElement>(null);
+  const bottomContentRef = useRef<HTMLDivElement>(null);
 
-const BottomNavbar = () => {
-    const [active, setActive] = useState("");
-    const [currenData, setCurrenData] = useState(data[active] ? data[active].options : []);
-    const [drawerOpen, setDrawerOpen] = useState(false);
-    const drawerRef = useRef<HTMLDivElement>(null);
-    const bottomContentRef = useRef<HTMLDivElement>(null);
-
-    const selectOption = (option: string) => {
-        if (option === active) {
-            setDrawerOpen(false);
-            setActive("");
-        } else {
-            setActive(option);
-            setCurrenData(data[option].options);
-            setDrawerOpen(true);
-        }
+  const selectOption = (option: string) => {
+    if (option === active) {
+      setDrawerOpen(false);
+      setActive("");
+    } else {
+      setActive(option);
+      setCurrenData(data[option].options);
+      setDrawerOpen(true);
     }
+  };
 
-    const boldText = (text: string) => {
-        const primeraPalabra = text.split(" ")[0]; // Obtener la primera palabra del texto
-        const restoDelTexto = text.slice(primeraPalabra.length); // Obtener el resto del texto
-
-        return (
-            <>
-                <b>{primeraPalabra}</b>
-                {restoDelTexto}
-            </>
-        );
-    };
-
-    const handleDrawerOptionClick = () => {
-        setDrawerOpen(false);
-        setActive("");
-    };
-
-    const handleClickOutside = (event: any) => {
-        if (drawerRef.current && !drawerRef.current.contains(event.target) &&
-            bottomContentRef.current && !bottomContentRef.current.contains(event.target)) {
-            setDrawerOpen(false);
-            setActive("");
-        }
-    };
-
-    useEffect(() => {
-        document.addEventListener("mousedown", handleClickOutside);
-        return () => {
-            document.removeEventListener("mousedown", handleClickOutside);
-        };
-    }, []);
-
+  const boldText = (text: string) => {
+    const primeraPalabra = text.split(" ")[0]; // Obtener la primera palabra del texto
+    const restoDelTexto = text.slice(primeraPalabra.length); // Obtener el resto del texto
 
     return (
-        <div className="container">
-            <div ref={drawerRef} className={`drawer ${drawerOpen ? 'expanded' : 'collapsed'}`}>
-                {
-                    currenData.map((option) => (
-                        <div key={option.text} className="drawer-option" onClick={() => handleDrawerOptionClick()}>
-                            <div className="drawer-option-circle">
-                                <option.icon />
-                            </div>
-                            <span className="drawer-option-text">{boldText(option.text)}</span>
-                        </div>
+      <>
+        <b>{primeraPalabra}</b>
+        {restoDelTexto}
+      </>
+    );
+  };
 
-                    ))
-                }
-            </div>
-            <div ref={bottomContentRef} className="bottom_content">
-                <div className={`nav-item  ${active === "sell" && 'active'}`} onClick={() => selectOption("sell")}>
-                    <SellIcon />
-                    <span>hola</span>
-                </div>
-                <div className={`nav-item  ${active === "finance" ? 'active' : ''}`} onClick={() => selectOption("finance")}>
-                    <FinanceIcon />
-                    <span>Finance</span>
-                </div>
-                <div className={`nav-item  ${active === "shop" ? 'active' : ''}`} onClick={() => selectOption("shop")}>
-                    <ShopIcon />
-                    <span>Shop</span>
-                </div>
-                <div className={`nav-item  ${active === "chat" ? 'active' : ''}`} onClick={() => selectOption("chat")}>
-                    <ChatIcon />
-                    <span>Chat</span>
-                </div>
-                <div className={`nav-item  ${active === "more" ? 'active' : ''}`} onClick={() => selectOption("more")}>
-                    <MoreIcon />
-                    <span>More</span>
-                </div>
+  const handleDrawerOptionClick = () => {
+    setDrawerOpen(false);
+    setActive("");
+  };
 
+  const handleClickOutside = (event: any) => {
+    if (
+      drawerRef.current &&
+      !drawerRef.current.contains(event.target) &&
+      bottomContentRef.current &&
+      !bottomContentRef.current.contains(event.target)
+    ) {
+      setDrawerOpen(false);
+      setActive("");
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <div className="container">
+      <div
+        ref={drawerRef}
+        className={`drawer ${drawerOpen ? "expanded" : "collapsed"}`}
+      >
+        {currenData.map((option) => (
+          <div
+            key={option.text}
+            className="drawer-option"
+            onClick={() => handleDrawerOptionClick()}
+          >
+            <div className="drawer-option-circle">
+              <option.icon />
             </div>
+            <span className="drawer-option-text">{boldText(option.text)}</span>
+          </div>
+        ))}
+      </div>
+      <div ref={bottomContentRef} className="bottom_content">
+        <div
+          className={`nav-item  ${active === "sell" && "active"}`}
+          onClick={() => selectOption("sell")}
+        >
+          <SellIcon />
+          <span>Sell/Trade</span>
         </div>
-    )
-}
+        <div
+          className={`nav-item  ${active === "finance" ? "active" : ""}`}
+          onClick={() => selectOption("finance")}
+        >
+          <FinanceIcon />
+          <span>Finance</span>
+        </div>
+        <div
+          className={`nav-item  ${active === "shop" ? "active" : ""}`}
+          onClick={() => selectOption("shop")}
+        >
+          <ShopIcon />
+          <span>Shop</span>
+        </div>
+        <div
+          className={`nav-item  ${active === "chat" ? "active" : ""}`}
+          onClick={() => selectOption("chat")}
+        >
+          <ChatIcon />
+          <span>Chat</span>
+        </div>
+        <div
+          className={`nav-item  ${active === "more" ? "active" : ""}`}
+          onClick={() => selectOption("more")}
+        >
+          <MoreIcon />
+          <span>More</span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default BottomNavbar
+export default BottomNavbar;
