97197c48efdca0b8fcacdac2dd4673fa607f3c41
juanpablohernandez02 <jphernandez.v0211@gmail.com>
2023-11-24 11:02:28 -0500

    cambios en de animacion

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
+import React, { useState } from "react";
 import BottomNavbar from "./BottomNavbar/BottomNavbar";
-import './App.css'
+import "./App.css"; // Asegúrate de importar el archivo CSS aquí
+import image from "./img/carmax.jpg";
 
 function App() {
+  const [isNavbarExpanded, setIsNavbarExpanded] = useState(false);
+
+  // Función de devolución de llamada para manejar el cambio de active
+  const handleActiveChange = (activeValue: any) => {
+    setIsNavbarExpanded(activeValue);
+    console.log("activeValue", isNavbarExpanded);
+  };
 
   return (
-    <>
-      <BottomNavbar />
-    </>
-  )
+      <div>
+      <div className={`app-container ${isNavbarExpanded ? "blurred" : ""}`}>
+        <img src={image} alt="dfasd" width={400} height={700} />
+      </div>
+      <BottomNavbar onActiveChange={handleActiveChange} />
+      </div>
+    
+  );
 }
 
-export default App
+export default App;
