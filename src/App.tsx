963888cb53e584e8b20f521358dd6f8f182b242c
Yoselyn Castillo <yose.castillo06@gmail.com>
2023-11-24 12:43:48 -0400

    add buttons

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,12 @@ function App() {
   return (
     <div className="web ">
       <div className="versions">
-        <button onClick={() => setVersion("V1")}>Version 1</button>
-        <button onClick={() => setVersion("V2")}>Version 2</button>
-        <button onClick={() => setVersion("V3")}>Version 3</button>
-        <button onClick={() => setVersion("V4")}>Version 4</button>
+        <button className={version === "V1" ? "activeVersion" : ""} onClick={() => setVersion("V1")}>Version 1</button>
+        <button className={version === "V2" ? "activeVersion" : ""} onClick={() => setVersion("V2")}>Version 2</button>
+        <button className={version === "V3" ? "activeVersion" : ""} onClick={() => setVersion("V3")}>Version 3</button>
+        <button className={version === "V4" ? "activeVersion" : ""} onClick={() => setVersion("V4")}>Version 4</button>
       </div>
+
       {version === "V1" && <BottomNavbar />}
       {version === "V2" && <BottomNavbarV2 />}
       {version === "V3" && <BottomNavbarV2 />}
