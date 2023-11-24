c2ec3df0e5898df99902fc80fd3ef69a5eedca73
edixon-Lizcano <abatar.111@gmail.com>
2023-11-24 13:54:29 -0500

    end changes for drawer and line

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 // import BottomNavbar from "./BottomNavbar/BottomNavbar";
-import BottomNavbarItem from "../src/BottomNavbar/BottomNavbarItem/BottomNavbarItem";
+import BottomNavbarV3 from "../src/BottomNavbar/BottomNavbarV3/BottomNavbarV3";
 import './App.css'
-
+import { useState } from "react";
+import image from './assets/icons/IMG_3845 (1).jpg';
 function App() {
+  const [drawerOpen, setDrawerOpen] = useState(false);
 
   return (
-    <>
-      {/* <BottomNavbar /> */}
-      <BottomNavbarItem />
-    </>
+    <div className="App">
+      <div className={drawerOpen ? 'blur-background' : ''}>
+        <img src={image} alt="dfasd" width={400} height={750} />
+      </div>
+      <BottomNavbarV3 setDrawerOpen={setDrawerOpen} drawerOpen={drawerOpen}/>
+    </div>
   )
 }
 
