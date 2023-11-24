f18a26297500d2e8e2eb4ef6ebf6a8aa54276d55
Yoselyn Castillo <yose.castillo06@gmail.com>
2023-11-24 09:10:18 -0400

    wip 24-11

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import BottomNavbar from "./BottomNavbar/BottomNavbar";
+import BottomNavbarV2 from "./BottomNavbarV2/BottomNavbar";
+// import BottomNavbar from "./BottomNavbar/BottomNavbar";
 import './App.css'
 
 function App() {
 
   return (
-    <>
-      <BottomNavbar />
-    </>
+    <div className="web ">
+      <span>                  </span>
+      {/* <BottomNavbar /> */}
+      <BottomNavbarV2 />
+    </div>
   )
 }
 
