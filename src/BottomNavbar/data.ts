72ae2fc5edbcd8cb49f65438731d5f19cd3b4609
edixon-Lizcano <abatar.111@gmail.com>
2023-11-27 09:11:27 -0500

    No fund

--- src/BottomNavbar/data.ts
+++ src/BottomNavbar/data.ts
@@ -1,147 +1,151 @@
-import { BrowseIcon, ViewAllIcon, SellIcon, AttachMoneyIcon,
-    FinanceIcon,
-    ShopIcon,
-    ChatIcon,
-    MoreIcon,
+import {
+  BrowseIcon,
+  ViewAllIcon,
+  SellIcon,
+  AttachMoneyIcon,
+  FinanceIcon,
+  ShopIcon,
+  ChatIcon,
+  MoreIcon,
 } from "../assets/icons";
 
 type DataType = {
-    [key: string]: {
-      options: {
-        icon: any;
-        text: string;
-        url: string;
-      }[];
-    };
+  [key: string]: {
+    options: {
+      icon: any;
+      text: string;
+      url: string;
+    }[];
   };
+};
 
 export const data: DataType = {
-    sell:{
-        options:[
-            {
-                icon:BrowseIcon,
-                text: "Browse by category",
-                url: "https://lit.dev/"
-            },
-            {
-                icon:ViewAllIcon,
-                text: "View all inventory",
-                url: "https://lit.dev/"
-            },
-            {
-                icon:SellIcon,
-                text: "Shop cars near me",
-                url: "https://lit.dev/"
-            },
-            {
-                icon: AttachMoneyIcon,
-                text: "Find a store",
-                url: "https://lit.dev/"
-            },
-        ]
-    },
-    finance:{
-        options:[
-            {
-                icon: BrowseIcon,
-                text: "Browse by category",
-                url: "https://lit.dev/"
-            },
-            {
-                icon: ViewAllIcon,
-                text: "View all inventory",
-                url: "https://lit.dev/"
-            },
-            // {
-            //     icon: "browser",
-            //     text: "Shop cars near me",
-            //     url: "https://lit.dev/"
-            // },
-            // {
-            //     icon: "browser",
-            //     text: "Find a store",
-            //     url: "https://lit.dev/"
-            // },
-        ]
-    },
-    shop:{
-        options:[
-            {
-                icon: BrowseIcon,
-                text: "Browse by category",
-                url: "https://lit.dev/"
-            },
-            // {
-            //     icon: "browser",
-            //     text: "View all inventory",
-            //     url: "https://lit.dev/"
-            // },
-            // {
-            //     icon: "browser",
-            //     text: "Shop cars near me",
-            //     url: "https://lit.dev/"
-            // },
-            // {
-            //     icon: "browser",
-            //     text: "Find a store",
-            //     url: "https://lit.dev/"
-            // },
-        ]
-    },
-    chat:{
-        options:[
-            {
-                icon: BrowseIcon,
-                text: "Browse by category",
-                url: "https://lit.dev/"
-            },
-            {
-                icon: ViewAllIcon,
-                text: "View all inventory",
-                url: "https://lit.dev/"
-            },
-            {
-                icon: SellIcon,
-                text: "Shop cars near me",
-                url: "https://lit.dev/"
-            },
-            // {
-            //     icon: "browser",
-            //     text: "Find a store",
-            //     url: "https://lit.dev/"
-            // },
-        ]
-    },
-    more:{
-        options:[
-            {
-                icon: BrowseIcon,
-                text: "Browse by category",
-                url: "https://lit.dev/"
-            },
-            {
-                icon: ViewAllIcon,
-                text: "View all inventory",
-                url: "https://lit.dev/"
-            },
-            {
-                icon: SellIcon,
-                text: "Shop cars near me",
-                url: "https://lit.dev/"
-            },
-            {
-                icon: AttachMoneyIcon,
-                text: "Find a store",
-                url: "https://lit.dev/"
-            },
-        ]
-    },
-}
+  sell: {
+    options: [
+      {
+        icon: BrowseIcon,
+        text: "Browse by category",
+        url: "https://lit.dev/",
+      },
+      {
+        icon: ViewAllIcon,
+        text: "View all inventory",
+        url: "https://lit.dev/",
+      },
+      {
+        icon: SellIcon,
+        text: "Shop cars near me",
+        url: "https://lit.dev/",
+      },
+      {
+        icon: AttachMoneyIcon,
+        text: "Find a store",
+        url: "https://lit.dev/",
+      },
+    ],
+  },
+  finance: {
+    options: [
+      {
+        icon: BrowseIcon,
+        text: "Browse by category",
+        url: "https://lit.dev/",
+      },
+      {
+        icon: ViewAllIcon,
+        text: "View all inventory",
+        url: "https://lit.dev/",
+      },
+      // {
+      //     icon: "browser",
+      //     text: "Shop cars near me",
+      //     url: "https://lit.dev/"
+      // },
+      // {
+      //     icon: "browser",
+      //     text: "Find a store",
+      //     url: "https://lit.dev/"
+      // },
+    ],
+  },
+  shop: {
+    options: [
+      {
+        icon: BrowseIcon,
+        text: "Browse by category",
+        url: "https://lit.dev/",
+      },
+      // {
+      //     icon: "browser",
+      //     text: "View all inventory",
+      //     url: "https://lit.dev/"
+      // },
+      // {
+      //     icon: "browser",
+      //     text: "Shop cars near me",
+      //     url: "https://lit.dev/"
+      // },
+      // {
+      //     icon: "browser",
+      //     text: "Find a store",
+      //     url: "https://lit.dev/"
+      // },
+    ],
+  },
+  chat: {
+    options: [
+      {
+        icon: BrowseIcon,
+        text: "Browse by category",
+        url: "https://lit.dev/",
+      },
+      {
+        icon: ViewAllIcon,
+        text: "View all inventory",
+        url: "https://lit.dev/",
+      },
+      {
+        icon: SellIcon,
+        text: "Shop cars near me",
+        url: "https://lit.dev/",
+      },
+      // {
+      //     icon: "browser",
+      //     text: "Find a store",
+      //     url: "https://lit.dev/"
+      // },
+    ],
+  },
+  more: {
+    options: [
+      {
+        icon: BrowseIcon,
+        text: "Browse by category",
+        url: "https://lit.dev/",
+      },
+      {
+        icon: ViewAllIcon,
+        text: "View all inventory",
+        url: "https://lit.dev/",
+      },
+      {
+        icon: SellIcon,
+        text: "Shop cars near me",
+        url: "https://lit.dev/",
+      },
+      {
+        icon: AttachMoneyIcon,
+        text: "Find a store",
+        url: "https://lit.dev/",
+      },
+    ],
+  },
+};
 
 export const Array = [
-    { text: "Sell/Trade", icon: SellIcon },
-    { text: "Finance", icon: FinanceIcon },
-    { text: "Shop", icon: ShopIcon },
-    { text: "Chat", icon: ChatIcon },
-    { text: "More", icon: MoreIcon },
-  ];
+  { text: "sell", icon: SellIcon },
+  { text: "finance", icon: FinanceIcon },
+  { text: "shop", icon: ShopIcon },
+  { text: "chat", icon: ChatIcon },
+  { text: "more", icon: MoreIcon },
+];
